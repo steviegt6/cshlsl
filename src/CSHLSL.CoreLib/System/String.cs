132603a49248240fcbd6173d3a9a7565baba8b3a
Tomat <xxlennygamerxx@gmail.com>
2023-04-14 17:08:28 -0700

    Better obsolete message for String.

--- src/CSHLSL.CoreLib/System/String.cs
+++ src/CSHLSL.CoreLib/System/String.cs
@@ -1,4 +1,4 @@
 ﻿namespace System;
 
-[Obsolete("This class is not supported in CSHLSL.", true)]
+[Obsolete("This class is not supported in CSHLSL; HLSL has no concept of strings.", true)]
 public sealed class String { }
