8664fe24598c62d7a8157d27d85c909350ed3777
Tomat <xxlennygamerxx@gmail.com>
2023-04-19 16:43:05 -0700

    Guhhh fix issues with indexers.

---
+++ src/CSHLSL.CoreLib/System/Reflection/DefaultMemberAttribute.cs
@@ -0,0 +1,6 @@
+﻿namespace System.Reflection;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface)]
+public sealed class DefaultMemberAttribute : Attribute {
+    public DefaultMemberAttribute(string memberName) { }
+}
